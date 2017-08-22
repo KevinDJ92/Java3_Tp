fe8c2dedb2d67fa415e07cc728b3ee75b26e97c1
KevinDJ92 <kevinjohnston1992@hotmail.com>
2017-08-21 22:39:11 -0400

    Some commits weren't push. Trying to push them

--- src/java/com/manager/PreferencesManager.java
+++ src/java/com/manager/PreferencesManager.java
@@ -5,8 +5,7 @@ import com.service.ConnexionBD;
 import org.apache.ibatis.session.SqlSession;
 
 public class PreferencesManager {
-    
- public static void insertPreferences(Preferences preferences) {
+    public static void insertPreferences(Preferences preferences) {
         SqlSession session = ConnexionBD.getSession();
   
         session.insert("com.mapper.PreferencesMapper.insertPreferences", preferences);
